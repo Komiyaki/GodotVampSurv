f042e8714f5a347b72eb718b14d49f544751b46c
FurnitureSet <65529437+FurnitureSet@users.noreply.github.com>
2026-06-15 16:31:12 +0900

    Main scene connections

--- scenes/autoload/game_data.gd
+++ scenes/autoload/game_data.gd
@@ -1,14 +1,40 @@
 extends Node
 
+const DATA_PATH: String = "user://game_data.json"
+const SAVE_PATH: String = "user://save_data.json"
 
+const ENEMY_POOL: int = 100
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+    # _load_data()
     pass # Replace with function body.
 
+func _load_data() -> void:
+    if not FileAccess.file_exists(DATA_PATH):
+        push_error("No data file found at %s" % DATA_PATH)
+        return # Error! We don't have data to load.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+    var data_file = FileAccess.open(DATA_PATH, FileAccess.READ)
+    while data_file.get_position() < data_file.get_length():
+        print(data_file.get_line())
+        # var json_string = data_file.get_line()
+        # var json = JSON.new()
+
+        # var parse_result = json.parse(json_string)
+        # if not parse_result == OK:
+        #     push_error("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
+        #     continue
+
+        # var node_data = json.data
+
+func save_game() -> void:
+    pass
+
+func load_game() -> void:
+    if not FileAccess.file_exists("user://save_data.json"):
+        push_error("No save file found at %s" % SAVE_PATH)
+        return # Error! We don't have a save to load.
     pass
--- scenes/game/enemy_manager.gd
+++ scenes/game/enemy_manager.gd
@@ -4,9 +4,17 @@ class_name EnemyManager
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+    # instantiate a bunch of enemies to use
+    # signal ready up to gamemanager?
     pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+    pass
+
+func fill_enemy_pool() -> void:
+    pass
+
+func spawn_enemy() -> void:
     pass
